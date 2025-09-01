ef5dd9bd07f9a76210a52d5d8590cf9ee3300f2d
Hiral Chudasama <110782865+hiralchudasam@users.noreply.github.com>
2025-09-01 09:38:18 +0530

    Update main.py

--- Ai_Voice_Agent-main/main.py
+++ Ai_Voice_Agent-main/main.py
@@ -103,3 +103,4 @@ async def llm_query(request: Request, audio: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+nommmmmmmmmmmmm
